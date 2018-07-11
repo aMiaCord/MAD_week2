648c6d986cb946895bb140549bcadc7d9f155718
NorangBerry <hlnsud12@naver.com>
2018-07-12 07:08:21 +0900

    permission check

--- app/src/main/java/com/example/q/a2ndweek/LoginActivity.java
+++ app/src/main/java/com/example/q/a2ndweek/LoginActivity.java
@@ -1,6 +1,10 @@
 package com.example.q.a2ndweek;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -20,6 +24,7 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -33,8 +38,25 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-    }
+        ArrayList<String> permissions = new ArrayList<>();
+        permissions.add(checkPermission(Manifest.permission.READ_EXTERNAL_STORAGE));
+        permissions.add(checkPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE));
+        permissions.add(checkPermission(Manifest.permission.INTERNET));
+        while(permissions.remove(null));
+        if(!permissions.isEmpty())
+            getPermission(permissions.toArray(new String[permissions.size()]), permissions.size());
 
+    }
+    public void getPermission(String[] permissions,int request_code){
+        ActivityCompat.requestPermissions(this, permissions, request_code);
+    }
+    public String checkPermission(String request){
+        if(ContextCompat.checkSelfPermission(this,request) != PackageManager.PERMISSION_GRANTED){
+            return request;
+        }
+        else
+            return null;
+    }
     public void onLogin(View view){
         EditText idView = findViewById(R.id.idEditText);
         final String id = idView.getText().toString();
